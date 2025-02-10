f5d180a481140f3c4e3d7c63455a448114bf1d35
zartul <121471819+zartul@users.noreply.github.com>
2025-02-10 10:32:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -18,10 +18,10 @@ function convertToRoman(num) {
     let result = "";
 
     for (let i = 0; i<arr.length; i++) {
-       if(num>=arr[i][0]){
+       if(num>=arr[i][1]){
 		   result += arr[i][0];
-		num -= arr[i][1];
-		i--;
+			num -= arr[i][1];
+			i--;
 	   }
     }
     return result;
